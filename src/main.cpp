1058ab1985c44ecbbe1b1359524df4588906577c
markbonete <161759437+markbonete@users.noreply.github.com>
2024-06-13 11:46:34 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,50 @@
 #include <SD.h>
 
 File myFile;
-
-// put function declarations here:
-int myFunction(int, int);
 #define SS 40
+
 void setup() {
   Serial.begin(9600);
   SPI.begin(37,38,36);
   Serial.print("Iniciando SD ...");
   //begin(SS, SPI, 4000000, "/sd", 5, true);
   if (!SD.begin(SS, SPI, 4000000, "/sd", 5, true)) {
-  Serial.println("No se pudo inicializar");
-  return;
+    Serial.println("No se pudo inicializar la tarjeta SD.");
+    return;;
   }
-  Serial.println("inicializacion exitosa");
-  myFile = SD.open("archivo.txt");//abrimos el archivo
+  
+  
+  Serial.println("Inicialización exitosa.");
+
+  // Crear y escribir en el archivo en la tarjeta SD
+  myFile = SD.open("/archivo_copiado.txt", FILE_WRITE);
   if (myFile) {
-  Serial.println("archivo.txt:");
-  while (myFile.available()) {
-  Serial.write(myFile.read());
-  }
-  myFile.close(); //cerramos el archivo
+    Serial.println("Archivo creado correctamente.");
+
+    // Escribir datos en el archivo
+    myFile.println("¡Hola, mundo!");
+    myFile.println("Este es un archivo de texto creado desde Arduino.");
+
+    // Cerrar el archivo
+    myFile.close();
+    Serial.println("Datos escritos correctamente en el archivo.");
   } else {
-  Serial.println("Error al abrir el archivo");
+    Serial.println("Error al crear el archivo en la tarjeta SD.");
+  }
+
+  // Abrir y leer el archivo original desde la tarjeta SD
+  myFile = SD.open("/archivo_original.txt");//abrimos el archivo
+  if (myFile) {
+    Serial.println("/archivo_original.txt");
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    myFile.close(); //cerramos el archivo
+  } else {
+    Serial.println("Error al abrir el archivo.");
   }
 }
 
 void loop() {
-  
+ 
 }
